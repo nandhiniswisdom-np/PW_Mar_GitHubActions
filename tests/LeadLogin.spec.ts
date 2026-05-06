f33b327fc0e5bc083d609286c03aa2f30e8be571
Nandhini <nandhiniswisdom@gmail.com>
2026-05-06 20:51:56 +0530

    latest commit

--- tests/LeadLogin.spec.ts
+++ tests/LeadLogin.spec.ts
@@ -2,17 +2,13 @@ import {test} from '@playwright/test'
 test('Create lead and push code to GitHub', async({page})=>
 {
     //launching URL
- await page.goto("http://leaftaps.com/opentaps/control/main");
- //Entering username
- await page.locator('//input[@id="username"]').fill("Demosalesmanager");
- //entering password
- await page.locator('//input[@id="password"]').fill("crmsfa");
- //clcicking on login button
- await page.locator('//input[@class="decorativeSubmit"]').click();
- //await page.click('//input[@class="decorativeSubmit"]');
- //clicking on CRM/SFA button
+ await page.goto("https://leaftaps.com/opentaps/control/main");
+ await page.locator('#username').fill('democsr2')
+ await page.locator('#password').fill('crmsfa');
+ await page.locator('.decorativeSubmit').click();
  await page.locator('//a[contains(text(),"CRM")]').click();
- //clicking on Lead button
- await page.locator('//a[text()="Leads"]').click();
+ const Title= await page.title();
+ console.log(Title);
+
 
 })
