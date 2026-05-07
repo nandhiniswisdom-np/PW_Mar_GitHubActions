1b0336d1f425606f1dbe10c694a3ea9f1cd786a2
Nandhini <nandhiniswisdom@gmail.com>
2026-05-07 14:25:42 +0530

    Lets run

--- LFLogin.spec.ts
+++ LFLogin.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test'
 test('Login to LeafTap and push code to GitHub', async ({ page }) => {
   //launching URL
   await page.goto("https://leaftaps.com/opentaps/control/main");
-  await page.locator('#username').fill('democsr2')
+  await page.locator('#username').fill('democsr')
   await page.locator('#password').fill('crmsfa');
   await page.locator('.decorativeSubmit').click();
   await page.locator('//a[contains(text(),"CRM")]').click();
--- SFLogin.spec.ts
+++ SFLogin.spec.ts
@@ -2,8 +2,8 @@ import {test} from '@playwright/test'
 test("Login to SF and push the code to GitHub", async({page})=>
 {
     await page.goto('https://login.salesforce.com/');
-    await page.locator('#username').fill("nandhiniswisdom.af1b41b45784@agentforce.com");
-    await page.locator("#password").fill("Taara@2022");
+    await page.locator('#username').fill("dilipkumar.rajendran@testleaf.com");
+    await page.locator("#password").fill("TestLeaf@2025");
     await page.locator("#Login").click();
     await page.waitForTimeout(4000);
     console.log(page.url());
