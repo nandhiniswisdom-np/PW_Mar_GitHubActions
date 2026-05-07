e0b3538050da0593849d7aeee46c4d3633892ce8
Nandhini <nandhiniswisdom@gmail.com>
2026-05-07 13:32:53 +0530

    New Test file

---
+++ contactcreation.spec.ts
@@ -0,0 +1,20 @@
+import { test } from '@playwright/test'
+test('Create Contact and push code to GitHub', async ({ page }) => {
+  //launching URL
+  await page.goto("https://leaftaps.com/opentaps/control/main");
+  await page.locator('#username').fill('democsr2')
+  await page.locator('#password').fill('crmsfa');
+  await page.locator('.decorativeSubmit').click();
+  await page.locator('//a[contains(text(),"CRM")]').click();
+  //const Title = await page.title();
+  //console.log(Title);
+  console.log(page.url());
+  await page.locator('//a[text()="Leads"]').click();
+  await page.locator('//a[text()="Create Lead"]').click();
+  await page.locator('#createLeadForm_companyName').fill("CTS");
+  await page.locator('#createLeadForm_firstName').fill("Amudha");
+  await page.locator('#createLeadForm_lastName').fill("Guna");
+  await page.locator('.smallSubmit').click();
+  const LName = await page.locator('#viewLead_lastName_sp').innerText();
+  console.log(LName);
+})
