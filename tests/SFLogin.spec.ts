7a3808713f8a9399ba38d04e671df494ac58694b
Nandhini <nandhiniswisdom@gmail.com>
2026-05-07 12:59:24 +0530

    Todays run

---
+++ tests/SFLogin.spec.ts
@@ -0,0 +1,10 @@
+import {test} from '@playwright/test'
+test("Login to SF and push the code to GitHub", async({page})=>
+{
+    await page.goto('https://login.salesforce.com/');
+    await page.locator('#username').fill("nandhiniswisdom.af1b41b45784@agentforce.com");
+    await page.locator("#password").fill("Taara@2022");
+    await page.locator("#Login").click();
+    await page.waitForTimeout(5000);
+    console.log(page.url());
+})
